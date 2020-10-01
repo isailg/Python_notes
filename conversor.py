7265dfddd485dac4a785c10565160ab713b00aa5
isailg <ilogamx@gmail.com>
2020-10-01 12:56:43 -0500

    Initial commit

---
+++ conversor.py
@@ -0,0 +1,7 @@
+pesos = input ("How many MXN do you have?")
+pesos = float (pesos)
+usd_value = 21.92
+usd_u= pesos/usd_value
+usd_u= round (usd_u,2)
+usd_u = str(usd_u)
+print("You have $"+ usd_u+ "usd")

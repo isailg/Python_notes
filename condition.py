e4ee98f4f075d02f7fd0f9ac78b6bf2935163980
isailg <ilogamx@gmail.com>
2020-10-01 13:09:30 -0500

    Program 2 added

---
+++ condition.py
@@ -0,0 +1,16 @@
+# age = int(input("Type your age: "))
+
+# if age > 18:
+#     print("You are of age")
+# else:
+#     print("You are a minor")
+
+number = int(input("Type a number: "))
+
+if number > 5:
+    print("Greater of 5")
+elif number == 5:
+    print("Equal to 5")
+elif number < 5:
+    print("Less than 5")
+    
